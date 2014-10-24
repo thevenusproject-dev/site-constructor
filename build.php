f35d146bedd754206479cec0cdc71b2638e36345
Eugene Esca <scsmash3r@gmail.com>
2014-10-24 22:56:09 +0400

    Create build.php

---
+++ build.php
@@ -0,0 +1,8 @@
+<?php
+/*
+The Venus Project Site Sturcture Builder
+Authored by [SC]Smash3r for Venus Project activism needs
+Content generated from: https://github.com/thevenusproject-dev
+Tip: If you want to use it on your own host, start by launching this file. It will compile and prepare all the stuff, including latest backup.
+However, you will need setup CRON job for pulling the up-to-date site version manually.
+*/
